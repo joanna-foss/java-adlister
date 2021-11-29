d97d36ddc8c1e8828f5524d43fa8b5dbc422ef37
Joanna <m.joanna.foss@gmail.com>
2021-11-29 14:51:20 -0600

    feat: in-prog servlets

---
+++ src/main/java/CountPageViews.java
@@ -0,0 +1,19 @@
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "CountPageViews", urlPatterns = "/count")
+public class CountPageViews extends HttpServlet {
+    private Integer counter = 0;
+    protected void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        getServletContext().log("service() called");
+        int internalCount;
+        synchronized (counter) {
+            counter++;
+            internalCount = counter;
+        }
+        res.getWriter().write("total page views: " + internalCount);
+    }
+}
