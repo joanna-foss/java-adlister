499f3e98e8dfa14573b0aa6d9b60324f495ebceb
Joanna <m.joanna.foss@gmail.com>
2021-11-29 15:18:12 -0600

    feat: added reset for queries

--- src/main/java/CountPageViews.java
+++ src/main/java/CountPageViews.java
@@ -6,20 +6,15 @@ import java.io.IOException;
 
 @WebServlet(name = "CountPageViews", urlPatterns = "/count")
 public class CountPageViews extends HttpServlet {
+
     private Integer counter = 0;
+
     protected void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
-//        int internalCount;
-//        String searchQuery = req.getParameter("q");
-//        synchronized (counter) {
-//            counter++;
-//            internalCount = counter;
-//            if(searchQuery != null) {
-//                internalCount = 1;
-//            }
-//        }
-//
-//        res.getWriter().write("total page views: " + internalCount);
+        String searchQuery = req.getParameter("q");
         counter++;
-        res.getWriter().println("count: " + counter);
+        if (searchQuery != null) {
+            counter = 1;
+        }
+        res.getWriter().write("total page views: " + counter);
     }
 }
