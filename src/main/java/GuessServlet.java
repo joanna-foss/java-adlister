4e691b61d7b1a3846878145eb6f4b9c9c166dd4c
Joanna <m.joanna.foss@gmail.com>
2021-12-02 14:00:48 -0600

    feat: started guess exercise

---
+++ src/main/java/GuessServlet.java
@@ -0,0 +1,17 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name="GuessServlet", urlPatterns = "/guess")
+public class GuessServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        req.getRequestDispatcher("/guess.jsp").forward(req, res);
+    }
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse res){
+
+    }
+}
---
+++ src/main/webapp/correct.jsp
@@ -0,0 +1,9 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Correct</title>
+</head>
+<body>
+<h1>You guessed correctly!</h1>
+</body>
+</html>
