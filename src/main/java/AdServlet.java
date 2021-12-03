659e4822030beeee8366b53ea38e1487af177ba0
Joanna <m.joanna.foss@gmail.com>
2021-12-03 12:34:41 -0600

    feat: completed mvc

---
+++ src/main/java/AdServlet.java
@@ -0,0 +1,14 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "AdServlet", urlPatterns = "/ads")
+public class AdServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        req.setAttribute("ads", DaoFactory.getAdsDao().all());
+        req.getRequestDispatcher("/ads/index.jsp").forward(req, res);
+    }
+}
---
+++ src/main/webapp/ads/index.jsp
@@ -0,0 +1,20 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <jsp:include page="/partials/head.jsp">
+        <jsp:param name="title" value="Welcome to my site!" />
+    </jsp:include>
+</head>
+<body>
+    <jsp:include page="/partials/navbar.jsp" />
+    <div class="container">
+        <h1>Welcome to the Adlister!</h1>
+
+        <c:forEach var="any" items="${ads}">
+            <h4>${any.title}</h4>
+            <p>${any.description}</p>
+        </c:forEach>
+    </div>
+</body>
+</html>
