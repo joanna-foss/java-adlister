09eeee129d4e93541ec9159d14cedc99bb0c2f0c
Joanna <m.joanna.foss@gmail.com>
2021-12-03 16:41:47 -0600

    feat: in-prog

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -8,15 +8,18 @@ import java.io.IOException;
 @WebServlet(name = "LoginServlet", urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/login.jsp").forward(request, response);
+        request.getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
         boolean validAttempt = username.equals("admin") && password.equals("password");
+        boolean user = false;
 
         if (validAttempt) {
+            request.getSession().setAttribute("isAdmin", true);
+            request.getSession().setAttribute("user", true);
             response.sendRedirect("/profile");
         } else {
             response.sendRedirect("/login");
--- src/main/java/ViewProfileServlet.java
+++ src/main/java/ViewProfileServlet.java
@@ -3,11 +3,19 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(name = "ViewProfileServlet", urlPatterns = "/profile")
 public class ViewProfileServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/profile.jsp").forward(request, response);
+        HttpSession current = request.getSession();
+        if(!(boolean) current.getAttribute("user")) {
+            response.sendRedirect("/WEB-INF/login.jsp");
+        } else if ((boolean) current.getAttribute("user")) {
+            request.getRequestDispatcher("/WEB-INF/profile.jsp").forward(request, response);
+        } else {
+            request.getRequestDispatcher("/WEB-INF/profile.jsp").forward(request, response);
+        }
     }
 }
